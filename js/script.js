4e7e5af0810188ff0647942441a0b2e829fb5a58
Bhagabati Prasad <bhagabatiprasada@gmail.com>
2021-06-30 00:12:39 +0530

    added navbar preloader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
+// preloader
+$(window).on('load', function () {
+  $('#preloader').delay(1050).fadeOut('slow');
+  $('body').delay(3850).css({ overflow: 'visible' });
+});
+
+// scroll header bg animation start
+$(document).ready(function () {
+  // scroll to top
+  $('#to_top').on('click', function () {
+    window.scrollTo(0, 1);
+  });
+  // navbar background toggle on scroll
+  $(window).scroll(function () {
+    let top = $(document).scrollTop();
+    if (top > 170) {
+      $('header nav').css('background', '#111');
+      $('header nav').addClass('shadow');
+      $('#to_top').addClass('active');
+    } else {
+      $('header nav').css('background', 'rgba(0, 0, 0, 0.9)');
+      $('header nav').removeClass('shadow');
+      $('#to_top').removeClass('active');
+    }
+  });
+});
+
+$(document).ready(function () {
+  // toggle mobile navigation
+  $('.toggle_btn').click(function () {
+    $('#mob-menu').slideDown('slow');
+    $('.mobnav ul').show();
+  });
+  $('#close').click(function () {
+    $('#mob-menu').slideUp('slow');
+  });
+  $('.mobnav ul li a').click(function () {
+    $('#mob-menu').fadeOut();
+  });
+});
+
 // owl carousel
 let owl = $('.owl-carousel');
 owl.owlCarousel({
@@ -29,3 +70,10 @@ owl.on('mousewheel', '.owl-stage', function (e) {
   }
   e.preventDefault();
 });
+
+// scroll to top when refreshed
+window.onload = function () {
+  setTimeout(function () {
+    window.scrollTo(0, 1);
+  }, 0);
+};
