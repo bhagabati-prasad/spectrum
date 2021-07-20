35220504631f8a1735caf121527b8adfd14deb3d
Bhagabati Prasad <bhagabatiprasada@gmail.com>
2021-07-20 20:15:27 +0530

    cleaned client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 function App() {
   return (
-    <>
-      <div className='App'>
-        <h1>Hello World!</h1>
-      </div>
-    </>
+    <div className='App'>
+      <h1>Hello World!</h1>
+    </div>
   );
 }
 
